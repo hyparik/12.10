8a43c3b723e9c9dd7f5042a90a64e84d2276fee3
hyparik <145677312+hyparik@users.noreply.github.com>
2024-10-12 12:13:10 +0500

    Add files via upload

---
+++ 12.10.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    cout << "Первое задание (6)"
+
+    int x, y, result;
+
+    cout << "Напишите число: ";
+    cin >> x;
+    result = x;
+    cout << "\nНапишите степень: ";
+    cin >> y;
+    
+    for (int i = 1; i < y; i++) {
+        result *= x;
+    }
+
+    cout << x << " ^ " << y << " = " << result;
+
+    cout << "Второе задание (14)"
+
+    int q = 0, min = 9;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        if (min > q % 10) {
+            min = q % 10;
+        }
+        q /= 10;
+    }
+    cout << min;
+}
+
